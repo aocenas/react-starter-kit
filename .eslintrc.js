532c48527809bbb725b2ad107417ab095670fe38
Andrej Ocenas <mr.ocenas@gmail.com>
2016-08-11 22:49:45 +0200

    updated eslintrc and some related packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
-    parser: "babel-eslint", // for some reason not picked up need to set it as arg to eslint
-    extends: ["eslint:recommended", "plugin:react/recommended"],
+    parser: 'babel-eslint', // for some reason not picked up need to set it as arg to eslint
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended'
+    ],
     parserOptions: {
         ecmaVersion: 6,
         sourceType: 'module',
@@ -12,7 +15,7 @@ module.exports = {
         node: true
     },
     plugins: [
-        "flow-vars",
+        'flowtype',
         'react'
     ],
     rules: {
@@ -21,10 +24,16 @@ module.exports = {
         'comma-dangle': [2, 'always-multiline'],
         // TODO maybe makes sense for client code, maybe different eslint conf for that
         'no-console': 0,
-        "flow-vars/define-flow-type": 1,
-        "flow-vars/use-flow-type": 1,
+        'flowtype/define-flow-type': 1,
+        'flowtype/use-flow-type': 1,
         'jsx-quotes': [1, 'prefer-double'],
-        quotes: [2, "single", {"avoidEscape": true, "allowTemplateLiterals": true}],
-        indent: ["error", 4, {SwitchCase: 1}]
+        quotes: [2, 'single', {'avoidEscape': true, 'allowTemplateLiterals': true}],
+        indent: ['error', 4, {SwitchCase: 1}],
+        'define-flow-type': 0
+    },
+    settings: {
+        flowtype: {
+            'onlyFilesWithFlowAnnotation': false
+        }
     }
 };
